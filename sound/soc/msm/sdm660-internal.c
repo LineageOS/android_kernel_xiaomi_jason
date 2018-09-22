8414fc96f18660ea14fe951602928506edda3924
dianlujitao <dianlujitao@lineageos.org>
2018-09-22 22:02:05 +0800

    ASoC: Import Xiaomi changes
    Change-Id: If840fc6a9c4471cbaa9560662abe3af47cb0a1c4

--- sound/soc/msm/sdm660-internal.c
+++ sound/soc/msm/sdm660-internal.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2015-2017, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1198,7 +1199,7 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 		return NULL;
 
 #define S(X, Y) ((WCD_MBHC_CAL_PLUG_TYPE_PTR(msm_int_wcd_cal)->X) = (Y))
-	S(v_hs_max, 1500);
+	S(v_hs_max, 1700);
 #undef S
 #define S(X, Y) ((WCD_MBHC_CAL_BTN_DET_PTR(msm_int_wcd_cal)->X) = (Y))
 	S(num_btn, WCD_MBHC_DEF_BUTTONS);
@@ -1223,14 +1224,14 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 	 */
 	btn_low[0] = 75;
 	btn_high[0] = 75;
-	btn_low[1] = 150;
-	btn_high[1] = 150;
-	btn_low[2] = 225;
-	btn_high[2] = 225;
-	btn_low[3] = 450;
-	btn_high[3] = 450;
-	btn_low[4] = 500;
-	btn_high[4] = 500;
+	btn_low[1] = 260;
+	btn_high[1] = 260;
+	btn_low[2] = 750;
+	btn_high[2] = 750;
+	btn_low[3] = 750;
+	btn_high[3] = 750;
+	btn_low[4] = 750;
+	btn_high[4] = 750;
 
 	return msm_int_wcd_cal;
 }
