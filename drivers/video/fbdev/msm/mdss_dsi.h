7d28f0befab348094572bd35f1bace5b2d41b2d0
dianlujitao <dianlujitao@lineageos.org>
2018-09-24 11:37:29 +0800

    msm: mdss: Import Xiaomi changes
    Change-Id: I9e8c7f54857c16f2ec7f8e7b94daabd72c4ff344

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -65,6 +66,8 @@
 #define MDSS_DSI_HW_REV_STEP_2		0x2
 
 #define NONE_PANEL "none"
+
+#define BUF_LEN_MAX    256
 
 enum {		/* mipi dsi panel */
 	DSI_VIDEO_MODE,
@@ -394,6 +397,12 @@ struct dsi_err_container {
 	s64 err_time[MAX_ERR_INDEX];
 };
 
+struct dsi_panel_read {
+	char cmd;
+	char param_num;
+	int params_bit; /*  bit:1 - this parameter need to return */
+};
+
 #define DSI_CTRL_LEFT		DSI_CTRL_0
 #define DSI_CTRL_RIGHT		DSI_CTRL_1
 #define DSI_CTRL_CLK_SLAVE	DSI_CTRL_RIGHT
@@ -460,6 +469,7 @@ struct mdss_dsi_ctrl_pdata {
 	int lcd_mode_sel_gpio;
 	int bklt_ctrl;	/* backlight ctrl */
 	enum dsi_ctrl_op_mode bklt_dcs_op_mode; /* backlight dcs ctrl mode */
+	u32 bklt_level;
 	bool pwm_pmi;
 	int pwm_period;
 	int pwm_pmic_gpio;
@@ -500,6 +510,18 @@ struct mdss_dsi_ctrl_pdata {
 	struct dsi_panel_cmds lp_on_cmds;
 	struct dsi_panel_cmds lp_off_cmds;
 	struct dsi_panel_cmds status_cmds;
+
+	struct dsi_panel_cmds dispparam_dimmingon_cmds;
+
+	struct dsi_panel_cmds displayoff_cmds;
+	struct dsi_panel_cmds displayon_cmds;
+	bool dsi_panel_off_mode;
+
+	struct dsi_panel_read xy_coordinate_cmds;
+	char panel_read_data[BUF_LEN_MAX];
+
+	struct delayed_work cmds_work;
+
 	u32 *status_valid_params;
 	u32 *status_cmds_rlen;
 	u32 *status_value;
